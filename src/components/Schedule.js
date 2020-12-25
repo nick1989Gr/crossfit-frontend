fb337905d9ed06e99ff7f950ead1f878cd733e8a
Nikolaos Ilieskou <nikoloas.ilieskou@signify.com>
2020-12-25 15:41:27 +0100

    Schedule component is updated. It does not show week numbers but just the week starting from current date.

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -12,14 +12,27 @@ import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import IconButton from "@material-ui/core/IconButton";
 import {
-  getWeekNumber,
-  getOneWeekBack,
-  getOneWeekForward,
   formatDate,
-  getWeekDates,
+  getPreviousWeekDate,
+  getNextWeekDate,
+  getWeekDays,
   NUM_WEEK_DAYS,
 } from "../utils/dateUtils";
 
+const WORKING_HOURS = [
+  "10:00",
+  "11:00",
+  "12:00",
+  "13:00",
+  "14:00",
+  "15:00",
+  "16:00",
+  "17:00",
+  "18:00",
+  "19:00",
+  "20:00",
+  "21:00",
+];
 const useStyles = makeStyles({
   container: {
     marginLeft: "auto",
@@ -37,7 +50,7 @@ const useStyles = makeStyles({
     top: 20,
   },
   tableHeader: {
-    backgroundColor: "#484648",
+    backgroundColor: "#9F3410",
   },
   headerCell: {
     color: "#FFFFFF",
@@ -48,8 +61,7 @@ const useStyles = makeStyles({
 
 export const Schedule = () => {
   const classes = useStyles();
-  const [weekInYear, setWeekInYear] = useState(getWeekNumber(new Date()));
-  console.log(weekInYear);
+  const [startDay, setStartDay] = useState(new Date());
 
   const getCallendarHeader = () => {
     return (
@@ -57,27 +69,26 @@ export const Schedule = () => {
         <TableRow>
           <TableCell></TableCell>
           <TableCell></TableCell>
+          <TableCell></TableCell>
           <TableCell className={classes.headerCell}>
             <IconButton
               edge="start"
               className={classes.menuButton}
               color="inherit"
               aria-label="menu"
-              onClick={() => setWeekInYear(getOneWeekBack(weekInYear))}
+              onClick={() => setStartDay(getPreviousWeekDate(startDay))}
             >
               <ArrowBackIcon />
             </IconButton>
           </TableCell>
-          <TableCell className={classes.headerCell}>
-            {weekInYear.weekNo}
-          </TableCell>
+          <TableCell className={classes.headerCell}> Week</TableCell>
           <TableCell className={classes.headerCell}>
             <IconButton
               edge="start"
               className={classes.menuButton}
               color="inherit"
               aria-label="menu"
-              onClick={() => setWeekInYear(getOneWeekForward(weekInYear))}
+              onClick={() => setStartDay(getNextWeekDate(startDay))}
             >
               <ArrowForwardIcon />
             </IconButton>
@@ -86,7 +97,8 @@ export const Schedule = () => {
           <TableCell></TableCell>
         </TableRow>
         <TableRow>
-          {getWeekDates(weekInYear.firstDay).map((d, i) => (
+          <TableCell className={classes.headerCell}>Time</TableCell>
+          {getWeekDays(startDay).map((d, i) => (
             <TableCell className={classes.headerCell} key={i}>
               {formatDate(d)}
             </TableCell>
@@ -96,15 +108,32 @@ export const Schedule = () => {
     );
   };
 
-  const getCallendarBody = () => {};
+  const getCallendarBody = () => {
+    return (
+      <>
+        {WORKING_HOURS.map((hour) => (
+          <TableRow key={hour}>
+            <TableCell>{hour}</TableCell>
+            {Array.from(Array(NUM_WEEK_DAYS), (_, index) => index + 1).map(
+              (i) => (
+                <TableCell key={i}>a</TableCell>
+              )
+            )}
+          </TableRow>
+        ))}
+      </>
+    );
+  };
 
   return (
-    <TableContainer className={classes.container}>
-      <Table className={classes.table} aria-label="simple table">
-        {getCallendarHeader()}
-      </Table>
-      <TableBody></TableBody>
-    </TableContainer>
+    <>
+      <TableContainer className={classes.container}>
+        <Table className={classes.table} aria-label="simple table">
+          {getCallendarHeader()}
+          <TableBody>{getCallendarBody()}</TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 };
 
--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -1,39 +1,4 @@
 export const NUM_WEEK_DAYS = 7;
-export const NUM_GYM_WORK_HOURS = 10;
-
-export function getWeekNumber(d) {
-  // Copy date so don't modify original
-  d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
-  // Set to nearest Thursday: current date + 4 - current day number
-  // Make Sunday's day number 7
-  d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
-  // Get first day of year
-  var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-  // Calculate full weeks to nearest Thursday
-  var weekNo = Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
-  // Return array of year and week number
-  const firstDay = new Date();
-  firstDay.setDate(d.getDate() - d.getDay() + 1);
-  const lastDay = new Date();
-  lastDay.setDate(d.getDate() - d.getDay() + 7);
-  return {
-    year: d.getUTCFullYear(),
-    weekNo,
-    firstDay,
-    lastDay,
-    reference: d,
-  };
-}
-
-export function getOneWeekForward(weekInYear) {
-  weekInYear.reference.setUTCDate(weekInYear.reference.getUTCDate() + 7);
-  return getWeekNumber(weekInYear.reference);
-}
-
-export function getOneWeekBack(weekInYear) {
-  weekInYear.reference.setUTCDate(weekInYear.reference.getUTCDate() - 7);
-  return getWeekNumber(weekInYear.reference);
-}
 
 // Formats date to Day dd Mon
 export function formatDate(d) {
@@ -52,18 +17,25 @@ export function formatDate(d) {
     "DEC",
   ];
   const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  console.log();
   return days[d.getDay()] + " " + d.getDate() + " " + months[d.getMonth()];
 }
 
-// Given a date it returns an array with 7 consequtive days starting
-// from the given date
-export function getWeekDates(firstWeekdayDate) {
-  let dates = [];
-  for (let i = 0; i < 7; i++) {
-    var d = new Date(firstWeekdayDate);
-    dates.push(d);
-    firstWeekdayDate.setUTCDate(firstWeekdayDate.getUTCDate() + 1);
+export function getWeekDays(startDay) {
+  let days = [];
+  days[0] = new Date(startDay);
+  for (let i = 1; i < 7; i++) {
+    days[i] = new Date();
+    days[i].setDate(startDay.getDate() + i);
   }
-  return dates;
+  return days;
+}
+
+export function getNextWeekDate(d) {
+  let ts = new Date().setDate(d.getDate() + NUM_WEEK_DAYS);
+  return new Date(ts);
+}
+
+export function getPreviousWeekDate(d) {
+  let ts = new Date().setDate(d.getDate() - NUM_WEEK_DAYS);
+  return new Date(ts);
 }
